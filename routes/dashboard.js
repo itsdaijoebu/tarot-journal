0f5e251e1faf3288a4c59aea1a70ac72b26a8b99
itsdaijoebu <joseph@josephchan.me>
2022-11-05 18:28:57 -0400

    fix card misposition on draw and grab pointer appearing on interpretation window
    cards were appearing under interpretation window when drawn due to default position of interp window being set in the draggable class, and pointer was being set to grab on draggable:hover

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,11 +4,8 @@ const dashboardController = require('../controllers/dashboard')
 const authController = require('../controllers/auth')
 const { ensureAuth } = require('../middleware/auth')
 
-// router.get('/', dashboardController.getReadings)
 router.get('/', ensureAuth, dashboardController.getDash)
 router.post('/save-reading', dashboardController.postReading)
 router.get('/readings', dashboardController.getReadings)
-// router.get('/readings', dashboardController.getReadings)
-// router.post('/readings', dashboardController.postReading)
 
 module.exports = router
